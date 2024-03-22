2f63927b742dfa73cd081e13d5285d4b9bf170d5
ankit kumar jha <2100032609@kluniversity.in>
2024-03-23 05:10:38 +0530

    file organization add

--- foundation/01_basics/main.go
+++ foundation/01_basics/main.go
@@ -1,22 +1,20 @@
 package main
 
-/* package main = project = workspace
+/* package main == project == workspace
 
-	Types of Packages
+Types of Packages
 
-	a. Executable => Generates a file that we can run
+a. Executable => Generates a file that we can run
 
-	eg, package main -> defines a package that can be compiled and then "executed".
-					Must have a func called 'main'.
+eg, package main -> defines a package that can be compiled and then "executed".
+				Must have a func called 'main'.
 
 
-	b. Reusable => Code used as 'helpers'. Good place to put reusable logic
+b. Reusable => Code used as 'helpers'. Good place to put reusable logic
 
-	eg, package ankit -> defines a package that can be used as a dependency code(helper code).
+eg, package ankit -> defines a package that can be used as a dependency code(helper code).
 
 */
-
-
 
 import "fmt"
 
@@ -27,7 +25,7 @@ import "fmt"
 
 	eg, reusable packages => other developer made
 
- */
+*/
 
 // comments
 
@@ -43,24 +41,32 @@ This is multiline comment.
 
 // For Production ready : first step to take, in integrated terminal: go mod init "specify name"
 
-
 /* Go CLI
 
-	go build => compiles a bunch of go source code files
+go build => compiles a bunch of go source code files
 
-	go run => compiles and executes one or two files
+go run => compiles and executes one or two files
 
-	go fmt => formats all the code in each file in the current dir
+go fmt => formats all the code in each file in the current dir
 
-	go install => compiles and "installs" a package
+go install => compiles and "installs" a package
 
-	go get => downloads the raw source code of someone else's package
+go get => downloads the raw source code of someone else's package
 
-	go test => runs any tests associated with the current project
+go test => runs any tests associated with the current project
 
 */
 
- */
+/*
+
+	file organization:
+		a) package main => package declaration
+		b) import "fmt" => import other packages that we need
+		c) func main(){
+			fmt.Println("Hey there")
+		}				=> declare functions, tell go to do things.
+*/
+
 func main() {
 	fmt.Println("Hey everyone")
 }
